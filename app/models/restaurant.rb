3c8a9fa115ed7894b6c8fe2839279444000beb77
antoine rochon <antoine.rochon@hello-houston.com>
2020-05-13 15:44:56 +0200

    full restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  validates :category, inclusion: { in: CATEGORY }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    
+
+# db/seeds.rb
+puts "Cleaning database..."
+
+puts "Creating restaurants..."
+le_Mandarin = { name: "Le Mandarin", address: "rue de la Paix", category: "chinese" }
+le_corbeau =  { name: "Le Corbeau", address: "Avenue Montaigne", category: "italian" }
+la_ficelle =  { name: "La Ficelle", address: "Bd Victor Hugo", category: "japonese" }
+a_la_bonne_franquette =  { name: "A la bonne franquette", address: "Time Square", category: "french" }
+chez_toto =  { name: "Chez Toto", address: "Time Square", category: "french" }
+
+[le_Mandarin, le_corbeau, la_ficelle, a_la_bonne_franquette, chez_toto].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
